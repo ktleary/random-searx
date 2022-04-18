204d7e4da0f88d60787bf030061c91433d6d9697
djlooop <djlooop@stringtalk.org>
2022-04-18 02:18:21 -0400

    filter by google rating

--- src/rndm-search.js
+++ src/rndm-search.js
@@ -11,20 +11,29 @@ const searchxInstancesPage = dev
 
 const statusOk = (status) => status > 199 && status < 300;
 
-const isGoodInstance = (http) =>
-	http && statusOk(http.status_code) && isGoodGrade(http.grade);
+const isGoodHttp = (instanceUrl) =>
+	instanceUrl && instanceUrl.indexOf("https") > -1;
 
-const getGoodSearchxInstances = (instances) =>
-	keys(instances).reduce(
-		(result, key) =>
-			isGoodInstance(instances[key].http) ? [...result, key] : result,
-		[]
-	);
+function googleWorks(engines, instanceUrl) {
+	return Number(engines?.google?.error_rate) < 1;
+}
+
+function isGoodInstance(instanceUrl, engines) {
+	return isGoodHttp(instanceUrl) && googleWorks(engines, instanceUrl);
+}
+
+function getGoodSearchxInstances(instances) {
+	return keys(instances).reduce((result, key) => {
+		const { engines } = instances[key];
+		return isGoodInstance(key, engines) ? [...result, key] : result;
+	}, []);
+}
 
 function processInstances(instances) {
 	const goodSearchxInstances = getGoodSearchxInstances(instances);
-	// document.location.href = getRandomListItem(goodSearchxInstances);
-	print(getRandomListItem(goodSearchxInstances));
+	goodSearchxInstances.forEach((instance) => console.log(instance));
+	document.location.href = getRandomListItem(goodSearchxInstances);
+	// print(getRandomListItem(goodSearchxInstances));
 }
 
 function redirectRandomSearx() {
